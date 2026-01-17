6bff0ec315c030492e73c3f1373934245ff1d215
Arpad-H <horvaarpad@gmail.com>
2026-01-17 12:19:14 +0100

    added bridge class t ovcs

---
+++ Assets/Scripts/Frontend/Bridge.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    public Material validBridgeMaterial;
+    public Material invalidBridgeMaterial;
+    public MeshRenderer bridgeMeshRenderer;
+    
+    public void SetValidMaterial(bool isValid)
+    {
+        if (isValid)
+        {
+            bridgeMeshRenderer.material = validBridgeMaterial;
+        }
+        else
+        {
+            bridgeMeshRenderer.material = invalidBridgeMaterial;
+        }
+    }
+}
